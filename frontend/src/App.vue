be4ca415e8d9d847fbfe6960af805ec769fc10d1
Bastien Andrey <bastien.andrey@edu.hefr.ch>
2024-11-20 14:43:49 +0100

    feat: add theme

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <v-app>
     <v-main>
-      <router-view />
+      <v-theme-provider theme="light">
+        <router-view />
+      </v-theme-provider>
     </v-main>
   </v-app>
 </template>
--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -7,13 +7,22 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import materialExport from "../material-theme-ihm2.json";
 
 // Composables
 import { createVuetify } from 'vuetify'
-
+console.log(materialExport)
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: 'light',
+    themes: {
+      "light": { dark: false, colors: materialExport.schemes.light },
+      "light-high-contrast": { dark: false, colors: materialExport.schemes['light-high-contrast'] },
+      "light-medium-contrast": { dark: false, colors: materialExport.schemes['light-medium-contrast'] },
+      "dark": { dark: true, colors: materialExport.schemes.dark },
+      "dark-high-contrast": { dark: true, colors: materialExport.schemes['dark-high-contrast'] },
+      "dark-medium-contrast": { dark: true, colors: materialExport.schemes['dark-medium-contrast'] },
+    }
   },
 })
