978e644fd299c235c21933d4c4c5eca7b39777cc
Baptiste Roland <baptiste.roland@hes-so.ch>
2024-12-18 14:42:24 +0100

    [feat] add slot

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,23 +1,33 @@
 <template>
-  <Form 
-  form-type="game" 
-  @submit="handleSubmit" 
-  @cancel="handleCancel"
-  />
-
-  <Form 
-    form-type="play" 
-    @submit="handleSubmit" 
-    @cancel="handleCancel"
-  />
-
-  <Form 
-    form-type="player" 
-    @submit="handleSubmit" 
-    @cancel="handleCancel"
-  />
+  <BaseForm @submit="handleSubmit" @cancel="handleCancel">
+    <template #default="{ formData, 'update:form-data': updateFormData }">
+      <FormAddPlay
+        :model-value="formData"
+        @update:model-value="updateFormData"
+      />
+      <FormAddGame
+        :model-value="formData"
+        @update:model-value="updateFormData"
+      />
+      <FormAddPlayer
+        :model-value="formData"
+        @update:model-value="updateFormData"
+      />
+    </template>
+  </BaseForm>
 </template>
 
 <script setup>
-  //
+import BaseForm from '../components/Forms/Form.vue'
+import FormAddPlay from '../components/Forms/FormAddPlay.vue'
+import FormAddGame from '../components/Forms/FormAddGame.vue'
+import FormAddPlayer from '../components/Forms/FormAddPlayer.vue'
+
+const handleSubmit = (formData) => {
+  console.log('Form submitted:', formData)
+}
+
+const handleCancel = () => {
+  console.log('Form cancelled')
+}
 </script>
