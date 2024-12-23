2ef5256e95d7491f5e980b080b204a4b3869a111
Baptiste Roland <baptiste.roland@hes-so.ch>
2024-12-23 16:15:07 +0100

    [feat] add base project

---
+++ parser/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+
+var client = new HttpClient();
+
+HttpResponseMessage response = await client.GetAsync (
+    "https://boardgamegeek.com/xmlapi/boardgame/224517");
+
+
+HttpContent responseContent = response.Content;
+
+using (var reader = new StreamReader(await responseContent.ReadAsStreamAsync()))
+{
+    Console.WriteLine(await reader.ReadToEndAsync());
+
+
+        // Create a file to write to.
+    string createText = "Hello and Welcome" + Environment.NewLine;
+    File.WriteAllText(path, createText);
+
+    ...
+
+    // Open the file to read from.
+    string readText = File.ReadAllText(path);
+}
