dd7dd82406649dd736ecf98905e48f69449945a2
Luca Michel <luca.michel@hes-so.ch>
2025-01-14 19:09:44 +0100

    [fix] added cors origins

--- backend/main.go
+++ backend/main.go
@@ -7,12 +7,17 @@ import (
 	"backend/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New(fiber.Config{
 		BodyLimit: 50 * 1024 * 1024,
 	})
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+	}))
 
 	err := config.Connect()
 	if err != nil {
